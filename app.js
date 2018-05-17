cc1463a3776ca2ad6614a2e134d1f86444a0f98f
tomasvrubio <tomasvrubio@gmail.com>
2018-05-17 05:59:10 +0200

    Comenzando con navbar

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var express = require('express');
-var handlebars = require('express-handlebars')
-        .create({ defaultLayout:'main' });
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main',    
+    helpers: {
+      static: function(name) {
+          return require('./lib/static.js').map(name);
+      }
+  }
+});
 
 var app = express();
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(req, res){
