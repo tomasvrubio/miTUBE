6f1b21c98e9700463b93cc819fd35ecf8b2e5e3c
tomasvrubio <tomasvrubio@gmail.com>
2018-05-24 05:57:54 +0200

    Introduciendo cookies y sesiones

--- app.js
+++ app.js
@@ -14,15 +14,19 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
+//Utilizamos un fichero con las credenciales. Importante que no sincronice con el repositorio.
+var credentials = require('./credentials.js');
 
 app.use(favicon(__dirname + '/public/favicon.png'));
 app.use(express.static(__dirname + '/public'));
-app.use(require('body-parser')());
-
+app.use(require('body-parser')()); //Para poder usar variables de formulario en req.body
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')());
 
 
 app.get('/', function(req, res){
-  var context = {name: "Cris",
+  var userName = req.session.userName || "Anonymous";
+  var context = {name: userName,
                   csrf: 'CSRF token goes here'};
   res.render('home', context);
 });
@@ -31,6 +35,7 @@ app.post('/process-home', function(req, res){
   console.log('Form (from querystring): ' + req.query.form);
   console.log('CSRF token (from hidden form field): ' + req.body._csrf);
   console.log('Name (from visible form field): ' + req.body.name);
+  req.session.userName = req.body.name;
   res.redirect(303, '/');
 });
 
@@ -75,6 +80,5 @@ app.use(function(err, req, res, next){
 
 
 app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' +
-    app.get('port') + ' press Ctrl-C to terminate.' );
+  console.log( 'Express started on http://localhost:' + app.get('port') + ' press Ctrl-C to terminate.' );
 });
