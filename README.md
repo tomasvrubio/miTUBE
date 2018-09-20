dc1ceb10a4c690599738606c7c8dd01b05d08148
Tomas Vazquez <tomasvrubio@gmail.com>
2018-09-20 06:58:04 +0200

    Demonio - Descarga canciones

--- README.md
+++ README.md
@@ -74,3 +74,5 @@ gmsync up -U "$mac_lista" -c $usuario_lista "data/$nombre_lista"
 
 * ¿Cómo se que un usuario se conecta por primera vez? Lo necesito para que esa primera vez sincronice con googleMusic y consiga su token de autenticación. Luego ya no se debe solicitar nunca más. Se me ocurre variable en BBDD que cargue cuando haga login y que luego ya se almacene en la información del usuario. ¿su cookie o tengo otro sitio mejor y que no esté viajando por la red?
   * Lo he puesto en el redirect del login satisfactorio. Lo que pasa es que si alguien se pone a navegar por los links de la cabecera de la aplicación sin haber sincronizado con gmusic no habrá nada que se lo impida. Darle una pensada!!
+
+* Para identificar las canciones que subo utilizar uno de los metadatos para almacenar el id de la canción. Mirar si realmente lo necesito pero puede ser una opción para después borrar canciones o al menos revisar si lo que tengo en la aplicación y lo que hay subido en google es lo mismo.
--- daemon.js
+++ daemon.js
@@ -1,11 +1,17 @@
 var nodemailer = require('nodemailer')
     mongoose = require('mongoose'),
+    fs = require('fs');
     credentials = require('./credentials.js');
+
+var WorkTodo = require('./models/workTodo.js');
 
 var Youtube = require('./lib/youtube.js')(),
     Synchronize = require('./lib/synchronize.js')(),
     Gmusic = require('./lib/gmusic.js')(),
     YoutubeDL = require('./lib/youtubedl.js')();
+
+//Database 
+mongoose.connect(credentials.mongo.connectionString);
 
 var active = 1; //To mantain de loop alive
 
@@ -17,26 +23,72 @@ const sleep = (milliseconds) => {
 //Declaration of the loop in async mode so we can call "await" inside to wait for promise end.
 async function loop() {
   do {
-    console.log("Soy el demonio encargado de trabajar.");
-    await sleep(2000); 
+    console.log("\n\nSoy el demonio encargado de trabajar.");
+    
+    //await WorkTodo.find({state:"upl"}, async function(err, uploads){ 
+    await WorkTodo.find({state:"upl"}).then(async function(uploads){ 
+      //Prueba para saber si hace las cosas en orden.
+      //await sleep(2500);
+      //console.log("He terminado en la función tras la query.")
+      
+      
+      if (uploads.length==0)
+        console.log("Sin subidas que realizar.");
+      else{
+        console.log("Hay que subir:")
+        console.log(uploads);
+        uploads.forEach(function(work){
+          if (fs.existsSync("./tmp/"+work.songId+".mp3")){
+            console.log("Existe el archivo para la canción " + work.songId);
+            //        Subir canción para los usuarios que haya trabajo registrado (será 1 o más). 
+            //        Con confirmación positiva eliminar mp3 y pasar trabajo de TODO a DONE. 
+          }
+          else{
+            console.log("No hay archivo para la canción " + work.songId);
+            work.state = "err";
+            work.dateLastMovement = Date.now();
+            //work.save();
+          } 
+        });
+      }
 
-    //Idea de como hacerlo.
-    //---------------------
-    //Consulto si hay trabajos pendientes de subir "upl"
-    //  Si hay: 
-    //    Comprobar que existe el mp3 en el directorio temporal.
-    //      Si hay:
-    //        Subir canción para los usuarios que haya trabajo registrado (será 1 o más). Con confirmación                positiva eliminar mp3 y pasar trabajo de TODO a DONE (borrar registro y crearlo en la otra                  tabla) 
-    //      No hay: 
-    //        Poner el trabajo en "err" y continuar comprobando el resto de trabajos en "upl".
-    //  No hay:
-    //    Consulto si hay trabajos pendientes de descargar "new"
-    //      Si hay:
-    //        Descargar canción de trabajo más antiguo. Marcar trabajo como "upl". 
-    //        Continuar sin descargar más (Para no acumular archivos en el directorio y que se pueda llenar)
-    //      No hay: 
-    //           
+      console.log("Se ha modificado uploads tras el forEach??");
+      console.log(uploads);
+      //uploads.save();
 
+      //await WorkTodo.findOne({state:"new"}, async function(err, work){ 
+      await WorkTodo.findOne({state:"new"}).then(async function(work){ 
+        if (work.length==0)
+          console.log("No hay descargas que realizar. Dormir durante X segundos.");
+          //await sleep(2000); 
+        else {
+          console.log("Hay que bajar la siguiente canción:");
+          console.log(work);
+
+          //await YoutubeDL.download(work.songId).then(returnObject => {
+          //const returnObject = await YoutubeDL.download(work.songId);
+
+          // await Promise.all([ YoutubeDL.download(work.songId) ]).then( ([returnObject]) => {
+          await YoutubeDL.download(work.songId).then(returnObject => {
+            if (returnObject == 0) {
+              console.log("Canción descargada.");
+              //Marcar trabajo como "upl".
+              work.state = "upl";
+              work.dateLastMovement = Date.now();
+              work.save();
+            }
+            else if (returnObject == 1)
+              console.log("No se ha podido descargar la canción.");
+          }).catch(err => {
+            console.error(err.stack);
+          });           
+        }
+      });
+    });
+
+    //  SI HUBIESE ALGÚN ERROR DEMASIADO GRAVE SALIR DEL BUCLE Y FINALIZAR DEMONIO?
+    
+    await sleep(2000); //Esta espera tengo que quitarla y dejar la de arriba.
   } while (active);
 }
 
